dcd5ef7ff9e09542274a58c9c2fefca8c89cf60f
kindtek@github.com <n8@kindtek.io>
2023-04-27 00:58:29 -0700

    pad

--- linux/build-kernel.sh
+++ linux/build-kernel.sh
@@ -171,7 +171,7 @@ printf "
 ==================================================================
 ==================================================================
 ==================================================================
-" "$($linux_kernel_version | tr ' ' )"
+" "$(echo $linux_kernel_version | tr ' ' )"
 # wget https://github.com/openzfs/zfs/releases/download/zfs-$zfs_version/zfs-$zfs_version.tar.gz
 if [ $5 != "" ] && [ $4 = "" ]; then
     echo "  install kernel when finished?

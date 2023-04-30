d9ebc2bb4707e33d872d1b64776870b2bdbce426
kindtek@github.com <n8@kindtek.io>
2023-04-30 03:19:32 -0700

    add one letter shortcuts

--- linux/clean.sh
+++ linux/clean.sh
@@ -1,9 +1,15 @@
+basic_dir="linux-build-msft"
+latest_dir="linux-build-torvalds"
+stable_dir="linux-build-gregkh"
+zfs_dir="zfs-build"
+
+
 if [ "$1" != "" ]; then
     clean_target=""
     arg1=$1
 fi
 
-while [ "$clean_target" = "" ]; do
+while [ "${clean_target}" = "" ]; do
 
 if [ "$arg1" = "" ]; then
         echo "
@@ -27,13 +33,13 @@ if [ "$arg1" = "" ]; then
     press ENTER to exit or choose something to clean
 
 "
-    if [ -d "linux-build-msft/.git" ]; then
+    if [ -d "$basic_dir/.git" ]; then
         echo "      - basic"
     fi
-    if [ -d "linux-build-gregkh/.git" ]; then
+    if [ -d "$stable_dir/.git" ]; then
         echo "      - stable"
     fi
-    if [ -d "linux-build-torvalds/.git" ]; then
+    if [ -d "$latest_dir/.git" ]; then
         echo "      - latest"
         echo "      - latest-rc"
 
@@ -51,41 +57,35 @@ else
     clean_target=$arg1
 fi
 
-    if [ "$clean_target" = "basic" ]; then
-        cd linux-build-msft
+    if [ "${clean_target,,}" = "basic" ] || [ "${clean_target,,}" = "b" ]; then
+        cd $basic_dir
         git reset --hard
         git clean -fxd
         cd ..
     fi
-    if [ "$clean_target" = "stable" ]; then
-        cd linux-build-gregkh
+    if [ "${clean_target,,}" = "stable" ] || [ "${clean_target,,}" = "s" ]; then
+        cd $stable_dir
         git reset --hard
         git clean -fxd
         cd ..
     fi
-    if [ "$clean_target" = "latest" ]; then
-        cd linux-build-torvalds
+    if [ "${clean_target,,}" = "latest" ] || [ "${clean_target,,}" = "l" ]; then
+        cd $latest_dir
         git reset --hard
         git clean -fxd
         cd ..
     fi
-    if [ "$clean_target" = "latest-rc" ]; then
-        cd linux-build-torvalds
+    if [ "${clean_target,,}" = "zfs" ] || [ "${clean_target,,}" = "z" ]; then
+        cd $zfs_dir
         git reset --hard
         git clean -fxd
         cd ..
     fi
-    if [ "$clean_target" = "zfs" ]; then
-        cd zfs-build
-        git reset --hard
-        git clean -fxd
-        cd ..
-    fi
-if [ "$clean_target" = "k-cache" ]; then
+    if [ "${clean_target,,}" = "k-cache" ] || [ "${clean_target,,}" = "k" ] ; then
         rm -rfv k-cache/*
         rm -rfv k-cache/.*
     fi
-    if [ "$clean_target" = "" ] || [ "$1" != "" ]; then
+    if [ "${clean_target,,}" = "" ] || [ "$1" != "" ]; then
         exit
     fi
     clean_target=""

5ea7c299ce50c0bba4626056cd616cdbea302e92
kindtek@github.com <n8@kindtek.io>
2023-05-03 22:53:34 -0700

    tarball fix

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -675,7 +675,7 @@ echo "
 " | tee "k-cache/wsl-kernel-install.ps1"
 cp "k-cache/wsl-kernel-install.ps1" "$win_k_cache/$ps_wsl_install_kernel_id"
 # rm "k-cache/$tarball_filename"
-tar -czvf "k-cache/$tarball_filename" -C k-cache k-cache
+tar -czvf "k-cache/$tarball_filename" -C k-cache .
 # work on *nix first
 mkdir -pv "$nix_k_cache" 2>/dev/null
 if [ -w "$nix_k_cache" ]; then

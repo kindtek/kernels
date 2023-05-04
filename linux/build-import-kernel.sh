4b9ff1f5805650137c55f9494a1cd6601566bfa9
kindtek@github.com <n8@kindtek.io>
2023-05-04 04:41:47 -0700

    syntax untangle

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -144,7 +144,7 @@ nix_k_cache=$HOME/k-cache
 
 # check that the user supplied source exists if not try to pick the best .config file available
 # user choice is best if it exists
-if [ ! "$config_source" = "" ] && [ -r "$config_source" ] && [ -s "$config_source" ]; then
+if [ "$config_source" != "" ] && [ -r "$config_source" ] && [ -s "$config_source" ]; then
     echo "config: $config_source
     
     

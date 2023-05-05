b1e10665580da03ec7fdb2faf621162a65b9046b
kindtek@github.com <n8@kindtek.io>
2023-05-05 16:57:56 -0700

    better arg handoff

--- linux/k-cache/wsl-kernel-install.ps1
+++ linux/k-cache/wsl-kernel-install.ps1
@@ -5,8 +5,9 @@ for ($i = 0; $i -lt $argArray.Length; $i += 1) {
     if ( "$paramValue" -eq "" ) {
         $argArray[$i] = "`"`""
     }
+    # write-host "param ${i}: $($argArray[$i])"
 }
 
 
 # wsl.exe -u agl -- $("$("/hal/dvlw/dvlp/docker/kali/make-kernel.sh $bashArgs")")
-wsl.exe --cd /hal/dvlw/dvlp/kernels/linux exec bash -c ./install-kernel.sh $argArray[0] $argArray[1] $argArray[2]
+wsl.exe --cd /hal/dvlw/dvlp/kernels/linux exec ./install-kernel.sh "$($argArray[0])" "$($argArray[1])" "$($argArray[2])"

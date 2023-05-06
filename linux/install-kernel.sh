0b6a34c4c9b71846951be326aea42e706adafa0a
kindtek@github.com <n8@kindtek.io>
2023-05-05 21:47:25 -0700

    refactor kernel install selection

--- linux/install-kernel.sh
+++ linux/install-kernel.sh
@@ -46,58 +46,20 @@ done
 win_k_cache="/mnt/c/users/$win_user/k-cache"
 mkdir -p "$win_k_cache"
 cd "$win_k_cache" || exit
-
-if [ "$2" != "" ] && [ "$2" != "latest" ] && [ ! -f "wsl-kernel-install_${2}_${3}.ps1" ]; then
-
-    while [ ! -f "$selected_kernel_install_file" ]; do
-        # only focus on single match if $2 has matches
-        latest_kernel_install_file="$(exec find . -maxdepth 1 -name "wsl-kernel-install_${2}_*" | head -n 1)"
-        if [ -f "$latest_kernel_install_file" ]; then
-            latest_kernel=$( echo "$latest_kernel_install_file" | sed -r -e "s/^\.\/wsl-kernel-install_(.*)_(.*)\.ps1$/\t\1_\2/g")
-        else
-            echo "
-kernels available to install:
-
-
-        name_timestamp
-        --------------------"
-            find . -name 'wsl-kernel-install_*' 2>/dev/null | sed -r -e "s/^wsl-kernel-install_(.*)_(.*)\.ps1$/\t\1_\2/g" | sort -r
-            echo "
-
-enter a kernel name to install:
-"
-            latest_kernel_install_file="$(ls -t1 wsl-kernel-install_* )"
-            read -r -p "
-($(echo "$latest_kernel_install_file" | sed -r -e "s/^wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/g"))
- " selected_kernel
-            if [ "${selected_kernel}" != "" ] && [ ! -f "wsl-kernel-install_$selected_kernel.ps1" ] && [ ! -f "$latest_kernel_install_file" ]; then
-                exit
-            elif [ -f "wsl-kernel-install_$selected_kernel.ps1" ]; then
-                selected_kernel_install_file="wsl-kernel-install_$selected_kernel.ps1"
-                echo "user entered ${selected_kernel_install_file} ..."
-            elif [ "$selected_kernel_install_file" = "" ] && [ -f "$latest_kernel_install_file" ]; then
-                echo "user confirmed ${latest_kernel_install_file} ..."
-                selected_kernel_install_file=$latest_kernel_install_file
-            fi
-        fi
-    done
-elif [ -f "wsl-kernel-install_${2}_${3}.ps1" ]; then
-    selected_kernel_install_file="wsl-kernel-install_${2}_${3}.ps1"
-    latest_kernel=$( echo "$selected_kernel_install_file" | sed -nr "s/^wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/p")
-
-elif [ "$2" = "latest" ]; then
-    selected_kernel_install_file="$(find . -name 'wsl-kernel-install_*' 2>/dev/null | head -n 1)"
-    latest_kernel=$( echo "$selected_kernel_install_file" | sed -nr "s/^wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/p")
+# if [ -f "wsl-kernel-install_${2}*_${3}*.ps1" ]; then
+if [ "${2}" = "latest" ]; then
+    selected_kernel_install_file="$(find . -name 'wsl-kernel-install_*' 2>/dev/null | sort -r | head -n 1)"
+    latest_kernel=$( echo "$selected_kernel_install_file" | sed -nr "s/^\.\/wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/p")
 
     echo "
 
 
-
+latest kernel found: $latest_kernel
 
 
 
     
-install $latest_kernel kernel into WSL or exit?"
+install kernel into WSL or exit?"
     read -r -p "
 (install $latest_kernel)
 " install_latest
@@ -110,14 +72,62 @@ no kernel install requested. exiting ...
         exit
     # else
     #     selected_kernel_install_file=$latest_kernel
-    fi
-else 
-    while [ ! -f "$selected_kernel_install_file" ]; do
-        # get latest kernel
-        latest_kernel_install_file="$(exec find . -maxdepth 1 -name "wsl-kernel-install_*" 2>/dev/null | sed -r -e "s/^\.\/wsl-kernel-install_(.*)_(.*)\.ps1$/\t\1_\2/g" | sort -r)"
+    fi 
+elif [ "${2}" != "" ] && [ "${3}" != "" ]; then
+    latest_kernel="$(find . -maxdepth 1 -name "wsl-kernel-install_${2}*_*${3}*.ps1" 2>/dev/null | sed -r -e "s/^\.\/wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/g" | sort -r | head -n 1)"
+    latest_kernel_install_file="wsl-kernel-install_${latest_kernel}.ps1"
+    selected_kernel_install_file=$latest_kernel_install_file
+    latest_kernel=$( echo "$selected_kernel_install_file" | sed -nr "s/^wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/p")  
+    read -r -p "
+(install $latest_kernel)
+"
+# elif [ "${2}" = "" ]; then
+#     while [ ! -f "$selected_kernel_install_file" ]; do
+#         latest_kernel="$(find . -maxdepth 1 -name 'wsl-kernel-install_*' 2>/dev/null | sed -r -e "s/^\.\/wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/g" | sort -r | head -n 1)"
+#         latest_kernel_install_file="wsl-kernel-install_${latest_kernel}.ps1"
+#         if [ -f "$latest_kernel_install_file" ]; then
+#             echo "
+# kernels available to install:
 
-        echo "
-kernels available to install:
+
+#         name_timestamp
+#         --------------------"
+#             find . -maxdepth 1 -name 'wsl-kernel-install_*' 2>/dev/null | sed -r -e "s/^\.\/wsl-kernel-install_(.*)_(.*)\.ps1$/\t\1_\2/g" | sort -r
+#             echo "
+
+# enter a kernel name to install:
+#     "
+#             read -r -p "
+# ($latest_kernel)
+#     " selected_kernel
+#             if [ "${selected_kernel}" != "" ] && [ ! -f "wsl-kernel-install_$selected_kernel.ps1" ] && [ ! -f "$latest_kernel_install_file" ]; then
+#                 exit
+#             elif [ "$selected_kernel" != "" ] && [ -f "wsl-kernel-install_$selected_kernel.ps1" ]; then
+#                 selected_kernel_install_file="wsl-kernel-install_$selected_kernel.ps1"
+#                 echo "user picked ${selected_kernel} ..."
+#             elif [ "$selected_kernel" = "" ] && [ -f "$latest_kernel_install_file" ]; then
+#                 echo "user confirmed ${latest_kernel} ..."
+#                 selected_kernel_install_file="wsl-kernel-install_$latest_kernel.ps1"
+#             fi
+#         fi
+#     done 
+else
+    # focus only on 2nd arg
+    while [ ! -f "$selected_kernel_install_file" ]; do
+        # only focus on single match if $2 has matches
+        
+        if [ "${2}" != "" ]; then
+            latest_kernel="$(find . -maxdepth 1 -name "wsl-kernel-install_${2}*_*" 2>/dev/null | sed -r -e "s/^\.\/wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/g" | sort -r | head -n 1)"
+            latest_kernel_install_file="wsl-kernel-install_${latest_kernel}.ps1"
+            output_msg="enter a kernel name to install:"
+        else
+            latest_kernel="$(find . -maxdepth 1 -name 'wsl-kernel-install_*' 2>/dev/null | sed -r -e "s/^\.\/wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/g" | sort -r | head -n 1)"
+            latest_kernel_install_file="wsl-kernel-install_${latest_kernel}.ps1"
+            output_msg="kernels available to install matching ${2}*:"
+        fi
+        if [ -f "$latest_kernel_install_file" ]; then
+            echo "
+$output_msg
 
 
         name_timestamp
@@ -127,44 +137,43 @@ kernels available to install:
 
 enter a kernel name to install:
 "
-        latest_kernel="$(find . -maxdepth 1 -name 'wsl-kernel-install_*' 2>/dev/null | sed -r -e "s/^\.\/wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/g" | head -n 1)"
-        read -r -p "
+            read -r -p "
 ($latest_kernel)
  " selected_kernel
-        if [ "${selected_kernel}" != "" ] && [ ! -f "wsl-kernel-install_$selected_kernel.ps1" ] && [ ! -f "wsl-kernel-install_$latest_kernel.ps1" ]; then
-            exit
-        elif [ -f "wsl-kernel-install_$selected_kernel.ps1" ]; then
-            selected_kernel_install_file="wsl-kernel-install_$selected_kernel.ps1"
-            echo "user entered ${selected_kernel} ..."
-        elif [ "$selected_kernel" = "" ] && [ -f "wsl-kernel-install_$latest_kernel.ps1" ]; then
-            echo "user confirmed ${latest_kernel} ..."
-            selected_kernel_install_file="wsl-kernel-install_$latest_kernel.ps1"
-        elif [ "$selected_kernel" = "" ]; then
-            echo "could not find ${latest_kernel} ..."
-            selected_kernel_install_file="wsl-kernel-install_$latest_kernel.ps1"
+            if [ "${selected_kernel}" != "" ] && [ ! -f "wsl-kernel-install_$selected_kernel.ps1" ] && [ ! -f "$latest_kernel_install_file" ]; then
+                exit
+            elif [ "$selected_kernel" != "" ] && [ -f "wsl-kernel-install_$selected_kernel.ps1" ]; then
+                selected_kernel_install_file="wsl-kernel-install_$selected_kernel.ps1"
+                echo "user picked ${selected_kernel} ..."
+            elif [ "$selected_kernel" = "" ] && [ -f "$latest_kernel_install_file" ]; then
+                echo "user confirmed ${latest_kernel_install_file} ..."
+                selected_kernel_install_file=$latest_kernel_install_file
+            fi
+        elif [ "${2}" != "" ]; then
+            echo "
+
+no kernels like ${2} found.
+
+enter a different kernel code to search or exit?
+            "
+            read -r -p "
+(exit)
+ " selected_kernel
+            if [ "$selected_kernel" = "" ]; then
+                exit
+            else
+                selected_kernel_install_file="wsl-kernel-install_$selected_kernel.ps1"
+            fi
         fi
     done
 fi
+
 
 if [ ! -f "$selected_kernel_install_file" ]; then
     echo "could not find $selected_kernel_install_file
 exiting ..."
 fi
 if [ -f "$selected_kernel_install_file" ]; then
-    wsl_config=../.wslconfig 
-    new_kernel=$( echo "$selected_kernel_install_file" | sed -nr "s/^wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/p")
-    old_kernel=$(sed -nr "s/^\s*\#*\s*kernel=(.*)\\\\\\\\([A-Za-z0-9_-]+)$/\2/p" "$wsl_config")
-    # make sure there actually was an old kernel before deleting
-    if [ "$old_kernel" != "" ]; then
-        rm -fv "$old_kernel" 
-        cp -v "wsl-kernel-install_$old_kernel.ps1"  "wsl-kernel-rollback.ps1"
-        rm -v ".config_$old_kernel"
-    else    
-        echo "
-move .wslconfig.old .wslconfig.new
-move .wslconfig .wslconfig.old
-move .wslconfig.new .wslconfig" | tee "wsl-kernel-rollback.ps1"
-    fi
     echo "
 
 
@@ -177,6 +186,20 @@ restart WSL when installation is complete?"
     read -r -p "
 (install $latest_kernel and restart)
 " restart_wsl
+    wsl_config=../.wslconfig 
+    new_kernel=$(echo "$selected_kernel_install_file" | sed -nr "s/^\.?\/?wsl-kernel-install_(.*)_(.*)\.ps1$/\1_\2/p")
+    old_kernel=$(sed -nr "s/^\s*\#*\s*kernel=(.*)\\\\\\\\([A-Za-z0-9_-]+)$/\2/p" "$wsl_config")
+    # make sure there actually was an old kernel before deleting
+    if [ "$old_kernel" != "" ]; then
+        rm -fv "$old_kernel" 
+        cp -v "wsl-kernel-install_$old_kernel.ps1"  "wsl-kernel-rollback.ps1"
+        rm -v ".config_$old_kernel"
+    else    
+        echo "
+move .wslconfig.old .wslconfig.new
+move .wslconfig .wslconfig.old
+move .wslconfig.new .wslconfig" 2>/dev/null | tee "wsl-kernel-rollback.ps1"
+    fi
     if [ "$restart_wsl" = "" ]; then
         echo "running:  $selected_kernel_install_file restart"
         pwsh -file "$selected_kernel_install_file" restart

dc445d45ed6aa6bcdcd7a380e41aba4e9db22f89
kindtek@github.com <n8@kindtek.io>
2023-05-07 05:32:11 -0700

    cleanup/refactor

--- linux/k-cache/wsl-restart.ps1
+++ linux/k-cache/wsl-restart.ps1
@@ -11,118 +11,100 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
     }
 # }
 # catch {}
+Write-Host "
 
-write-host "
-attempting to restart wsl ..."
+"
+write-host -n "attempting to restart wsl"
 
 
 if ($IsLinux) {
-    write-host "... from within WSL Linux distro
+    write-host -n " from within WSL Linux distro"
+    Write-Output "
+    stopping docker ... 
 "
-    Write-Output "stopping docker ..."
     # & cmd.exe /c net stop com.docker.service
     # Start-Process -FilePath cmd.exe -ArgumentList '/c net stop com.docker.service' -NoNewWindow
 
-    # pwsh {    
-        # Stop-Service -Name "com.docker.service" -Force
-        Start-Process -FilePath cmd.exe -ArgumentList '/c net stop com.docker.service' -NoNewWindow
-        $process = Get-Process -Name "Docker Desktop" -ErrorAction SilentlyContinue
-        if ($process) {
-            Stop-Process -Id $process.Id -Force
-        }
-        else {
-            Write-Host "The process 'Docker Desktop' was not found."
-        }
-        Write-Output "restarting wsl ..."
-        Start-Process -FilePath wsl.exe -ArgumentList  '--shutdown'; Start-Process -FilePath wsl.exe -ArgumentList  '--exec echo "wsl restarted"';
-        # powershell.exe -Command "wsl.exe --shutdown; wsl.exe --exec echo 'wsl restarted'"
-        # bash -c systemctl restart systemd-shim
-        # Write-Output "starting docker ..."
-        # Start-Sleep -Seconds 5 # wait for 5 seconds to ensure that the service has stopped
-        # Start-Process -FilePath cmd.exe -ArgumentList '/c net start com.docker.service' -NoNewWindow
-    
-        Write-Output "starting docker ..."
-        Start-Service -Name com.docker.service
-        # powershell.exe -Command cmd.exe /c net start com.docker.service
-        # & net start com.docker.service
-        Start-Process -FilePath wsl.exe -ArgumentList  '--exec echo "docker restarted"'
+    # Stop-Service -Name "com.docker.service" -Force
+    Start-Process -FilePath cmd.exe -ArgumentList '/c net stop com.docker.service' -NoNewWindow
+    $process = Get-Process -Name "Docker Desktop" -ErrorAction SilentlyContinue
+    if ($process) {
+        Stop-Process -Id $process.Id -Force
+    }
+    else {
+        Write-Host "
+The process 'Docker Desktop' was not found.
+"
+    }
+    Write-Output "
+restarting wsl ...
+"
+    Start-Process -FilePath wsl.exe -ArgumentList  '--shutdown'; Start-Process -FilePath wsl.exe -ArgumentList  '--exec echo "wsl restarted"';
+    # powershell.exe -Command "wsl.exe --shutdown; wsl.exe --exec echo 'wsl restarted'"
+    # bash -c systemctl restart systemd-shim
+    # Write-Output "starting docker ..."
+    # Start-Sleep -Seconds 5 # wait for 5 seconds to ensure that the service has stopped
+    # Start-Process -FilePath cmd.exe -ArgumentList '/c net start com.docker.service' -NoNewWindow
+    Write-Output "
+starting docker ...
+"
+    # Start-Service -Name com.docker.service
+    Start-Process -FilePath cmd.exe -ArgumentList '/c net stop com.docker.service' -NoNewWindow
+    # powershell.exe -Command cmd.exe /c net start com.docker.service
+    # & net start com.docker.service
+    Start-Process -FilePath wsl.exe -ArgumentList  '--exec echo "docker restarted"'
 
-        # Find the installation path of Docker Desktop
-        $dockerPath = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" `
-        | Where-Object { $_.DisplayName -eq "Docker Desktop" } `
-        | Select-Object -ExpandProperty InstallLocation
-
-        # Start Docker Desktop if it's installed
-        if ($dockerPath) {
-            & "$dockerPath\Docker Desktop.exe"
-        }
-        else {
-            Write-Error "Docker Desktop is not installed on this machine."
-        }
-    # }
 
 
 }
 else {
     if ($IsWindows) {
-        write-host "... from within native Windows
+        write-host -n " from within native Windows
 "
 
     } 
     else {
-        write-host "... from within unlabeled environment
+        write-host -n " from within unlabeled environment
 "
     }
 
-    Write-Output "stopping docker ..."
+    Write-Output "
+    
+stopping docker ...
+"
     # & powershell.exe -Command "Start-Process -FilePath 'net' -ArgumentList 'stop', 'com.docker.service' -Verb RunAs"
     & net stop com.docker.service
     Stop-Process -Name "Docker Desktop" -Force
-    Write-Output "restarting wsl ..."
+    Write-Output "
+restarting wsl ...
+"
     powershell.exe -Command wsl.exe --shutdown; powershell.exe -Command wsl.exe --exec echo 'wsl restarted'
 
     
-    Write-Output "starting docker ..."
+    Write-Output "
+starting docker ...
+"
     powershell.exe -Command cmd.exe /c net start com.docker.service
     # & net start com.docker.service
-    Start-Process -FilePath wsl.exe -ArgumentList  '--exec echo "docker restarted"'
-
-    # Find the installation path of Docker Desktop
-    $dockerPath = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" `
-    | Where-Object { $_.DisplayName -eq "Docker Desktop" } `
-    | Select-Object -ExpandProperty InstallLocation
-
-    # Start Docker Desktop if it's installed
-    if ($dockerPath) {
-        & "$dockerPath\Docker Desktop.exe"
-    }
-    else {
-        Write-Error "Docker Desktop is not installed on this machine."
-    }
 
 
 
 }
 
+# Find the installation path of Docker Desktop
+$dockerPath = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" `
+| Where-Object { $_.DisplayName -eq "Docker Desktop" } `
+| Select-Object -ExpandProperty InstallLocation
 
-# Write-Output "starting docker ..."
-# powershell.exe -Command cmd.exe /c net start com.docker.service
-# # & net start com.docker.service
-# Start-Process -FilePath wsl.exe -ArgumentList  '--exec echo "docker restarted"'
-
-# # Find the installation path of Docker Desktop
-# $dockerPath = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" `
-# | Where-Object { $_.DisplayName -eq "Docker Desktop" } `
-# | Select-Object -ExpandProperty InstallLocation
-
-# # Start Docker Desktop if it's installed
-# if ($dockerPath) {
-#     & "$dockerPath\Docker Desktop.exe"
-# }
-# else {
-#     Write-Error "Docker Desktop is not installed on this machine."
-# }
-
+# Start Docker Desktop if it's installed
+if ($dockerPath) {
+    & "$dockerPath\Docker Desktop.exe"
+}
+else {
+    Write-Error "
+Docker Desktop is not installed on this machine.
+"
+}
 
 
 
@@ -136,6 +118,6 @@ Write-Host "
 
 
 
-(done)
+(next)
 " -NoNewline
 $BookPrice = Read-Host

7a6cd27957249de2dca3e1accf693cfe2498acac
kindtek@github.com <n8@kindtek.io>
2023-05-06 22:17:23 -0700

    more flexible restart process hopefully

--- linux/k-cache/wsl-restart.ps1
+++ linux/k-cache/wsl-restart.ps1
@@ -1,20 +1,9 @@
 # Self-elevate the privileges if required
-try {
-    if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-        if ((Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-            $CommandLine = '-File "{0}" {1}' -f $MyInvocation.MyCommand.Path, $MyInvocation.UnboundArguments
-            Start-Process -FilePath powershell.exe -Verb Runas -WindowStyle Maximized -ArgumentList $CommandLine
-            Exit
-        }
-    }
-}
-catch {
-    Write-Host "not admin - try restarting anyways?"
-     $confirm_restart=Read-Host -Prompt "
-(restart WSL)
-"
-    if ("$confirm_restart" -ne ""){
-        exit
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -FilePath PowerShell.exe -Verb Runas -WindowStyle "Maximized" -ArgumentList $CommandLine
+        Exit
     }
 }
 
@@ -25,14 +14,25 @@ attempting restart ..."
 if ($IsLinux) {
     write-host "... from within WSL Linux distro
 "
-    Write-Output "stopping docker ..."
-    Start-Process -FilePath cmd.exe -ArgumentList '/c net stop com.docker.service'
-    Start-Process -FilePath cmd.exe -ArgumentList '/c taskkill /IM "Docker Desktop.exe" /F'
-    Write-Output "restarting wsl ..."
-    Start-Process -FilePath wsl.exe -ArgumentList '--shutdown'; pwsh Start-Process -FilePath wsl.exe -ArgumentList '--exec echo "wsl restarted"';
-    Write-Output "starting docker ..."
-    Start-Process -FilePath cmd.exe -ArgumentList '/c net start com.docker.service'
-    Start-Process -FilePath wsl.exe -ArgumentList  '--exec echo "docker restarted"';
+
+
+    # Write-Output "stopping docker ..."
+    # & cmd.exe /c net stop com.docker.service
+    # # Start-Process -FilePath cmd.exe -ArgumentList '/c net stop com.docker.service' -NoNewWindow
+
+    # # Stop-Service -Name "com.docker.service" -Force
+    # $process = Get-Process -Name "Docker Desktop" -ErrorAction SilentlyContinue
+    # if ($process) {
+    #     Stop-Process -Id $process.Id -Force
+    # }
+    # else {
+    #     Write-Host "The process 'Docker Desktop' was not found."
+    # }
+    # Write-Output "restarting wsl ..."
+    # powershell.exe -Command "wsl.exe --shutdown; wsl.exe --exec echo 'wsl restarted'"
+    # Write-Output "starting docker ..."
+    # Start-Sleep -Seconds 5 # wait for 5 seconds to ensure that the service has stopped
+    # Start-Process -FilePath cmd.exe -ArgumentList '/c net start com.docker.service' -NoNewWindow
 }
 else {
     if ($IsWindows) {
@@ -45,14 +45,43 @@ else {
 "
     }
 
-    Write-Output "stopping docker ..."
-    powershell.exe -Command cmd.exe /c net stop com.docker.service
-    powershell.exe -Command cmd.exe /c taskkill /IM "'Docker Desktop.exe'" /F
-    Write-Output "restarting wsl ..."
-    powershell.exe -Command wsl.exe --shutdown; powershell.exe -Command wsl.exe --exec echo 'wsl restarted';
-    Write-Output "starting docker ..."
-    powershell.exe -Command cmd.exe /c net start com.docker.service
-    powershell.exe -Command wsl.exe --exec echo 'docker restarted';
+    # Write-Output "stopping docker ..."
+    # # & powershell.exe -Command "Start-Process -FilePath 'net' -ArgumentList 'stop', 'com.docker.service' -Verb RunAs"
+    # & net stop com.docker.service
+    # Stop-Process -Name "Docker Desktop" -Force
+    # Write-Output "restarting wsl ..."
+    # powershell.exe -Command wsl.exe --shutdown; powershell.exe -Command wsl.exe --exec echo 'wsl restarted'
+
+
+}
+
+Write-Output "stopping lxss manager"
+net stop LxssManager
+
+Write-Output "stopping docker ..."
+# & powershell.exe -Command "Start-Process -FilePath 'net' -ArgumentList 'stop', 'com.docker.service' -Verb RunAs"
+& net stop com.docker.service
+Stop-Process -Name "Docker Desktop" -Force
+
+Write-Output "restarting wsl ..."
+powershell.exe -Command wsl.exe --shutdown; powershell.exe -Command wsl.exe --exec echo 'wsl restarted'
+
+Write-Output "starting docker ..."
+# powershell.exe -Command cmd.exe /c net start com.docker.service
+& net start com.docker.service
+Start-Process -FilePath wsl.exe -ArgumentList  '--exec echo "docker restarted"'
+
+# Find the installation path of Docker Desktop
+$dockerPath = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" `
+| Where-Object { $_.DisplayName -eq "Docker Desktop" } `
+| Select-Object -ExpandProperty InstallLocation
+
+# Start Docker Desktop if it's installed
+if ($dockerPath) {
+    & "$dockerPath\Docker Desktop.exe"
+}
+else {
+    Write-Error "Docker Desktop is not installed on this machine."
 }
 
 Read-Host "

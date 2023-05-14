8c807d93efd57ba5cb5cbef9f2c5d2d24e4f3545
kindtek@github.com <n8@kindtek.io>
2023-05-14 00:40:54 -0700

    set config - CONFIG_LOCALVERSION=kindtek-kernel-$kernel_alias_no_timestamp

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -311,6 +311,9 @@ win_user_home=/mnt/c/users/$win_user
 win_k_cache=$win_user_home/kache
 wsl_kernel=$win_k_cache/$kernel_alias
 wsl_config=$win_user_home/.wslconfig
+kindtek_kernel_version="kindtek-kernel-$kernel_alias_no_timestamp-"
+sed -i "s/#?\s?CONFIG_LOCALVERSION[ =].*/CONFIG_LOCALVERSION=${kindtek_kernel_version}/g" "$config_source"
+
 if [ "$win_user" = "" ]; then
     win_k_cache=""
     tarball_target_win=""

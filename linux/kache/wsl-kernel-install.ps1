4ecd320ae562bc318e85d4967ab32ba27087c3fd
kindtek@github.com <n8@kindtek.io>
2023-05-14 20:27:18 -0700

    use home dir of user

--- linux/kache/wsl-kernel-install.ps1
+++ linux/kache/wsl-kernel-install.ps1
@@ -3,8 +3,19 @@
 #         ./wsl-kernel-install 6L1 
 #         ./wsl-kernel-install 6L 2023
 
-
-Write-Host "wsl-kernel-install path: $($pwd)"
+$argString = $args -join " "
+$argArray = $argString.Split(" ")
+$win_user = ""
+if (!$IsLinux ) {
+    # get the user home dir info
+    $win_user = $Env:USERNAME
+    # write-host "win user is: `"$win_user`""
+}
+else {
+    write-host "could not find windows user"
+}
+# if it exists, prepend win_user info to front of array
+$args = @( "$win_user" ) + $args
 
 $args -split ' ' | ForEach-Object {
     if ([string]::IsNullOrEmpty($_)) {

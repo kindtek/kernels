b638135f77929169549cd37fcc8ccbb21247a563
kindtek@github.com <n8@kindtek.io>
2023-05-16 02:30:58 -0700

    depth

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -375,7 +375,7 @@ if ! (( quick_wsl_install )); then
     git reset --hard
     git clean -fxd
 fi
-linux_commit_checkout=$(git ls-remote "$linux_repo" --tags "$linux_kernel_version_tag" --short --depth=1 | grep -o '^[a-zA-Z0-9]*')
+linux_commit_checkout=$(git ls-remote "$linux_repo" --tags "$linux_kernel_version_tag" --short | grep -o '^[a-zA-Z0-9]*')
 if [ -d "$linux_build_dir/.git" ]; then
     cd "$linux_build_dir" || exit
     if ! (( quick_wsl_install )); then
@@ -384,13 +384,13 @@ if [ -d "$linux_build_dir/.git" ]; then
     fi
     echo "checking out $linux_kernel_version_tag - $linux_commit_checkout ..."
     # git checkout "tags/$linux_kernel_version_tag" -b "$kernel_alias" --progress
-    git fetch origin "$linux_commit_checkout" --progress --verbose
+    git fetch origin "$linux_commit_checkout"  --depth=1 --progress --verbose
     cd ..
 else
     echo "cloning $linux_kernel_version_tag ..."
     git clone $linux_repo --single-branch --branch "$linux_commit_checkout" --depth=1 --progress -- $linux_build_dir
 fi
-zfs_commit_checkout="$(git ls-remote "$zfs_repo" --tags "$zfs_version_tag" --short --depth=1 | grep -o '^[a-zA-Z0-9]*')"
+zfs_commit_checkout="$(git ls-remote "$zfs_repo" --tags "$zfs_version_tag" --short | grep -o '^[a-zA-Z0-9]*')"
 if [ "$zfs" = "zfs" ];  then
 #     echo "zfs == True
 # LINENO: ${LINENO}"
@@ -403,7 +403,7 @@ if [ "$zfs" = "zfs" ];  then
         echo "checking out $zfs_version_tag - $zfs_commit_checkout..."
 
         # git checkout "tags/$zfs_version_tag" -b "$kernel_alias" --progress
-        git fetch origin "$zfs_commit_checkout" --progress --verbose
+        git fetch origin "$zfs_commit_checkout"  --depth=1 --progress --verbose
         cd ..
     else
         echo "cloning $zfs_version_tag ..."

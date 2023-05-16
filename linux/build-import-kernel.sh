81990b3acecff0d6dd9e36e14d809cf8d000cbd6
kindtek@github.com <n8@kindtek.io>
2023-05-16 09:05:00 -0700

    remove unnecessary conditional statemtn

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -680,15 +680,15 @@ tee "kache/$ps_wsl_install_kernel_id" >/dev/null <<EOF
 
     # restart wsl (and install kernel/modules)
     if ("\$(\$args[1])" -ne ""){
-        if ("\$(\$args[1])" -ne "restart"){
-            echo "installing kernel to \$(\$args[1]) distro ..."
-            wsl.exe -d "\$(\$args[1])" --exec sudo cp -rfv "/mnt/c/users/\$env:USERNAME/kache/boot/\$kernel_version" / | tail -n 5
-            echo "installing kernel modules to \$(\$args[1]) distro ..."
-            wsl.exe --exec sudo cp -rfv "/mnt/c/users/\$env:USERNAME/kache/usr/src/\$kernel_version" / | tail -n 5
-            # order is important here for installing kernel headers
-            wsl.exe -d "\$(\$args[1])" --exec sudo yes 'y' | apt -y install "$linux_kernel_generic_header" 2>/dev/null
-            wsl.exe -d "\$(\$args[1])" --exec sudo yes 'y' | apt -y install "$linux_kernel_kali_header" 2>/dev/null
-        } elseif ("\$(\$args[1])" -eq "restart"){
+        
+        echo "installing kernel to \$(\$args[1]) distro ..."
+        wsl.exe -d "\$(\$args[1])" --exec sudo cp -rfv "/mnt/c/users/\$env:USERNAME/kache/boot/\$kernel_version" / | tail -n 5
+        echo "installing kernel modules to \$(\$args[1]) distro ..."
+        wsl.exe --exec sudo cp -rfv "/mnt/c/users/\$env:USERNAME/kache/usr/src/\$kernel_version" / | tail -n 5
+        # order is important here for installing kernel headers
+        wsl.exe -d "\$(\$args[1])" --exec sudo yes 'y' | apt -y install "$linux_kernel_generic_header" 2>/dev/null
+        wsl.exe -d "\$(\$args[1])" --exec sudo yes 'y' | apt -y install "$linux_kernel_kali_header" 2>/dev/null
+        if ("\$(\$args[1])" -eq "restart"){
             # restart wsl
             # pwsh -Command .\\wsl-restart.ps1;
             # Start-Process -FilePath powershell.exe -ArgumentList "-Command .\\wsl-restart.ps1"

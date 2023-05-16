0be656f3026cc881f746d6f567b3809923ef1e79
kindtek@github.com <n8@kindtek.io>
2023-05-16 09:23:11 -0700

    cleaner vars

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -703,9 +703,9 @@ tee "kache/$ps_wsl_install_kernel_id" >/dev/null <<EOF
         }
     } else {
             echo "installing kernel to default distro ..."
-            pwsh -Command wsl.exe --exec sudo cp -rfv "/mnt/c/users/\$env:USERNAME/kache/boot/${kindtek_kernel_version}${kindtek_kernel_suffix%-}" / | tail -n 5
+            pwsh -Command wsl.exe --exec sudo cp -rfv "/mnt/c/users/\$env:USERNAME/kache/boot/\$kernel_version" / | tail -n 5
             echo "installing kernel modules to default distro ..."
-            pwsh -Command wsl.exe --exec sudo cp -rfv "/mnt/c/users/\$env:USERNAME/kache/usr/src/${kindtek_kernel_version}${kindtek_kernel_suffix%-}" / | tail -n 5
+            pwsh -Command wsl.exe --exec sudo cp -rfv "/mnt/c/users/\$env:USERNAME/kache/usr/src/\$kernel_version" / | tail -n 5
             # order is important here for installing kernel headers
             pwsh -Command wsl.exe -d "\$(\$args[1])" --exec sudo yes 'y' | apt -y install "$linux_kernel_generic_header" 2>/dev/null
             pwsh -Command wsl.exe -d "\$(\$args[1])" --exec sudo yes 'y' | apt -y install "$linux_kernel_kali_header" 2>/dev/null

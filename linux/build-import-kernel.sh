b556043775c30dff6f1f67d949e15a0ec529cfb2
kindtek@github.com <n8@kindtek.io>
2023-05-16 05:24:05 -0700

    try no detach again

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -391,7 +391,7 @@ else
     git clone $linux_repo --single-branch --branch "$linux_commit_checkout" --depth=1 --progress -- $linux_build_dir
 fi
 cd "$linux_build_dir" || exit
-git checkout --detach
+git checkout
 cd .. || exit
 
 zfs_commit_checkout="$(git ls-remote "$zfs_repo" --tags "$zfs_version_tag" --short | grep -o '^[a-zA-Z0-9]*')"
@@ -414,7 +414,7 @@ if [ "$zfs" = "zfs" ];  then
         git clone "$zfs_repo" --single-branch --branch "$zfs_commit_checkout" --progress -- "$zfs_build_dir" 
     fi
     cd "$zfs_build_dir" || exit
-    git checkout --detach
+    git checkout
     cd .. || exit
 fi
 

88d738fb1bc8e77d8d938b8d95b3d0de2d2f5c07
kindtek@github.com <n8@kindtek.io>
2023-05-16 03:01:38 -0700

    depth

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -384,11 +384,11 @@ if [ -d "$linux_build_dir/.git" ]; then
     fi
     echo "fetching origin at $linux_kernel_version_tag - $linux_commit_checkout ..."
     # git checkout "tags/$linux_kernel_version_tag" -b "$kernel_alias" --progress
-    git fetch origin "$linux_commit_checkout" --depth=1 --progress --verbose
+    git fetch origin "$linux_commit_checkout" --depth=2 --progress --verbose
     cd ..
 else
     echo "cloning $linux_kernel_version_tag ..."
-    git clone $linux_repo --single-branch --branch "$linux_commit_checkout" --depth=1 --progress -- $linux_build_dir
+    git clone $linux_repo --single-branch --branch "$linux_commit_checkout" --depth=2 --progress -- $linux_build_dir
 fi
 git checkout
 zfs_commit_checkout="$(git rev-parse --short git ls-remote "$zfs_repo" --tags "$zfs_version_tag" --short | grep -o '^[a-zA-Z0-9]*')"
@@ -404,7 +404,7 @@ if [ "$zfs" = "zfs" ];  then
         echo "fetching origin at $zfs_version_tag - $zfs_commit_checkout..."
 
         # git checkout "tags/$zfs_version_tag" -b "$kernel_alias" --progress
-        git fetch origin "$zfs_commit_checkout" --depth=1 --progress --verbose
+        git fetch origin "$zfs_commit_checkout" --progress --verbose
         cd ..
     else
         echo "cloning $zfs_version_tag ..."

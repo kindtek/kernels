19f552dd7f36b67801fe6cf6f37d9218d0bcecbf
kindtek@github.com <n8@kindtek.io>
2023-05-16 03:26:18 -0700

    sleep

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -391,8 +391,9 @@ else
     git clone $linux_repo --single-branch --branch "$linux_commit_checkout" --depth=1 --progress -- $linux_build_dir
 fi
 cd "$linux_build_dir" || exit
-git status --porcelain | grep '^??' | cut -c4- >> .gitignore
-git add ".gitnore" && \
+git status --porcelain | grep '^??' | cut -c4- >> .gitignore && \
+sleep 5 && \
+git add ".gitnore" 
 git commit -m "fetched $kernel_alias" 
 git checkout -b "$kernel_alias" .
 cd .. || exit
@@ -418,6 +419,7 @@ if [ "$zfs" = "zfs" ];  then
     fi
     cd "$zfs_build_dir" || exit
     git status --porcelain | grep '^??' | cut -c4- >> .gitignore && \
+    sleep 5 && \
     git add ".gitnore"
     git commit -m "fetched $kernel_alias" 
     git checkout -b "$kernel_alias" .

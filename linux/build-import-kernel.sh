30ab2a3843e661439cbd1e37c12acad496c43806
kindtek@github.com <n8@kindtek.io>
2023-05-16 02:53:41 -0700

    short commit

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -375,7 +375,7 @@ if ! (( quick_wsl_install )); then
     git reset --hard
     git clean -fxd
 fi
-linux_commit_checkout=$(git ls-remote "$linux_repo" --tags "$linux_kernel_version_tag" --short | grep -o '^[a-zA-Z0-9]*')
+linux_commit_checkout="$(git rev-parse --short "$(git ls-remote "$linux_repo" --tags "$linux_kernel_version_tag" --short | grep -o '^[a-zA-Z0-9]*')")"
 if [ -d "$linux_build_dir/.git" ]; then
     cd "$linux_build_dir" || exit
     if ! (( quick_wsl_install )); then
@@ -391,7 +391,7 @@ else
     git clone $linux_repo --single-branch --branch "$linux_commit_checkout" --depth=1 --progress -- $linux_build_dir
 fi
 git checkout -b "$kindtek_kernel_version" "$linux_repo" "$linux_commit_checkout"
-zfs_commit_checkout="$(git ls-remote "$zfs_repo" --tags "$zfs_version_tag" --short | grep -o '^[a-zA-Z0-9]*')"
+zfs_commit_checkout="$(git rev-parse --short "$(git ls-remote "$zfs_repo" --tags "$zfs_version_tag" --short | grep -o '^[a-zA-Z0-9]*')")"
 if [ "$zfs" = "zfs" ];  then
 #     echo "zfs == True
 # LINENO: ${LINENO}"

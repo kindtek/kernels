89433a6b8abbb544c79a5ad3e211923cc798f0c1
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-03 04:38:40 -0700

    tar --overwrite

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -687,7 +687,7 @@ tee "kache/${ps_wsl_install_kernel_id}" >/dev/null <<EOF
     
     echo "extracting ${package_full_name_id}.tar.gz ..."
     # extract
-    tar -xzf "${package_full_name_id}.tar.gz"
+    tar --overwrite -xzf "${package_full_name_id}.tar.gz"
 
     echo "appending tail.wslconfig to .wslconfig"
     # append tail.wslconfig to .wslconfig
@@ -718,7 +718,7 @@ tee "kache/${ps_wsl_install_kernel_id}" >/dev/null <<EOF
         wsl.exe -d "\$(\$args[1])" --cd /mnt/c/users/\$win_user/kache --exec sudo apt-get -y upgrade;
         wsl.exe -d "\$(\$args[1])" --cd / --exec sudo apt-get -y install dwarves;
         wsl.exe -d "\$(\$args[1])" --cd /mnt/c/users/\$win_user/kache --exec sudo cp -fv ${package_full_name_id}.tar.gz; 
-        wsl.exe -d "\$(\$args[1])" --cd / --exec sudo tar -xzvf ${package_full_name_id}.tar.gz; 
+        wsl.exe -d "\$(\$args[1])" --cd / --exec sudo tar --overwrite -xzvf ${package_full_name_id}.tar.gz; 
         wsl.exe --cd / --exec sudo update-initramfs -u -k \${kernel_release}-dirty
         wsl.exe -d "\$(\$args[1])" --cd / --exec sudo apt-get -y install --reinstall dkms;
         wsl.exe -d "\$(\$args[1])" --cd / --exec sudo apt-get -y install --reinstall virtualbox;
@@ -734,7 +734,7 @@ tee "kache/${ps_wsl_install_kernel_id}" >/dev/null <<EOF
         wsl.exe --cd /mnt/c/users/\$win_user/kache --exec sudo apt-get -y upgrade;
         wsl.exe --cd / --exec sudo apt-get -y install dwarves;
         wsl.exe --cd /mnt/c/users/\$win_user/kache --exec sudo cp -fv ${package_full_name_id}.tar.gz; 
-        wsl.exe --cd / --exec sudo tar -xzvf ${package_full_name_id}.tar.gz;
+        wsl.exe --cd / --exec sudo tar --overwrite -xzvf ${package_full_name_id}.tar.gz;
         wsl.exe --cd / --exec sudo update-initramfs -u -k \${kernel_release}-dirty
         wsl.exe --cd / --exec sudo apt-get -y install --reinstall dkms;
         wsl.exe --cd / --exec sudo apt-get -y install --reinstall virtualbox;

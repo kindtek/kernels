47636063d37a6fcb9dc6037bbe40923c18814ef2
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-03 19:57:10 -0700

    cp tar to root

--- linux/build-import-kernel.sh
+++ linux/build-import-kernel.sh
@@ -717,7 +717,7 @@ tee "kache/${ps_wsl_install_kernel_id}" >/dev/null <<EOF
         wsl.exe -d "\$(\$args[1])" --cd /mnt/c/users/\$win_user/kache --exec sudo apt-get -y update; 
         wsl.exe -d "\$(\$args[1])" --cd /mnt/c/users/\$win_user/kache --exec sudo apt-get -y upgrade;
         wsl.exe -d "\$(\$args[1])" --cd / --exec sudo apt-get -y install dwarves;
-        wsl.exe -d "\$(\$args[1])" --cd /mnt/c/users/\$win_user/kache --exec sudo cp -fv ${package_full_name_id}.tar.gz; 
+        wsl.exe -d "\$(\$args[1])" --cd /mnt/c/users/\$win_user/kache --exec sudo cp -fv ${package_full_name_id}.tar.gz /; 
         wsl.exe -d "\$(\$args[1])" --cd / --exec sudo tar --overwrite -xzvf ${package_full_name_id}.tar.gz; 
         wsl.exe --cd / --exec sudo update-initramfs -u -k \${kernel_release}-dirty
         wsl.exe -d "\$(\$args[1])" --cd / --exec sudo apt-get -y install --reinstall dkms;
@@ -733,7 +733,7 @@ tee "kache/${ps_wsl_install_kernel_id}" >/dev/null <<EOF
         wsl.exe --cd /mnt/c/users/\$win_user/kache --exec sudo apt-get -y update; 
         wsl.exe --cd /mnt/c/users/\$win_user/kache --exec sudo apt-get -y upgrade;
         wsl.exe --cd / --exec sudo apt-get -y install dwarves;
-        wsl.exe --cd /mnt/c/users/\$win_user/kache --exec sudo cp -fv ${package_full_name_id}.tar.gz; 
+        wsl.exe --cd /mnt/c/users/\$win_user/kache --exec sudo cp -fv ${package_full_name_id}.tar.gz /; 
         wsl.exe --cd / --exec sudo tar --overwrite -xzvf ${package_full_name_id}.tar.gz;
         wsl.exe --cd / --exec sudo update-initramfs -u -k \${kernel_release}-dirty
         wsl.exe --cd / --exec sudo apt-get -y install --reinstall dkms;

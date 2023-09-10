a487dae6740dc09bab335aca0a7e3f472aebe0cd
kindtek <98300737+kindtek@users.noreply.github.com>
2023-09-09 21:12:44 -0700

    future mod install if necessary

--- linux/build-export-kernel.sh
+++ linux/build-export-kernel.sh
@@ -720,7 +720,7 @@ tee "kache/${ps_wsl_install_kernel_id}" >/dev/null <<EOF
         wsl.exe -- sudo apt-get -y update; 
         wsl.exe -- sudo apt-get -y upgrade;        
         wsl.exe --cd \$win_user_dir/kache -- cp -fv ${package_full_name_id}.tar.gz /kache/${package_full_name_id}.tar.gz;
-        #     wsl.exe --user r00t --cd / -- sudo tar --no-overwrite-dir --keep-old-files -xzvfC "/kache/${package_full_name_id}.tar.gz ."
+        # wsl.exe --user r00t --cd / -- sudo tar --no-overwrite-dir --keep-old-files -xzvfC "/kache/${package_full_name_id}.tar.gz ."
         # wsl.exe --user r00t -- sudo bash dkms autoinstall --modprobe-on-install --kernelsourcedir "$LFS"
     } else {
         echo "installing kernel to \$wsl_distro distro ..."
